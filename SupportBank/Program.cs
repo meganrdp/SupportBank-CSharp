55846429a63ea1d55b3da3400a4ff92fb57fb317
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-19 13:36:41 +0100

    added error logging

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,9 +1,18 @@
 ﻿namespace SupportBank;
 
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
 internal class Program
 {
     static void Main(string[] args)
     {
+        var config = new LoggingConfiguration();
+        var target = new FileTarget { FileName = @"C:\Users\megan\OneDrive\Documents\Training\Boot Camp\SupportBank\Logs\supportbank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+        config.AddTarget("File Logger", target);
+        config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+        LogManager.Configuration = config;
         
         var bank = new Bank("C:\\Training\\Boot Camp\\SupportBank\\DodgyTransactions2015.csv");
 
