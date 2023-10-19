05895386e5000482a02a021d8948b97d38571e85
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-10-19 11:13:41 +0100

    with try and catch for date and amount

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -4,6 +4,12 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        
+        var bank = new Bank("C:\\Training\\Boot Camp\\SupportBank\\DodgyTransactions2015.csv");
 
+        bank.ListAll();
+
+        bank.ListAccount("Tim L");
+        
     }
 }
